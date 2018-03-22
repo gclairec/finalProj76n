2742e2de6757f964540abd361c469622b5eb9d3b
STUDENT <STUDENT@CEAC-TCs-iMac-20.local>
2018-03-22 13:27:11 +0800

    Added Initial XCode PageView Project

---
+++ CancioCongsonVerallo_76n/CancioCongsonVerallo_76n/DataViewController.h
@@ -0,0 +1,17 @@
+//
+//  DataViewController.h
+//  CancioCongsonVerallo_76n
+//
+//  Created by STUDENT on 22/03/2018.
+//  Copyright (c) 2018 Gillian Claire Cancio. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface DataViewController : UIViewController
+
+@property (strong, nonatomic) IBOutlet UILabel *dataLabel;
+@property (strong, nonatomic) id dataObject;
+
+@end
+
---
+++ CancioCongsonVerallo_76n/CancioCongsonVerallo_76n/ModelController.h
@@ -0,0 +1,19 @@
+//
+//  ModelController.h
+//  CancioCongsonVerallo_76n
+//
+//  Created by STUDENT on 22/03/2018.
+//  Copyright (c) 2018 Gillian Claire Cancio. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@class DataViewController;
+
+@interface ModelController : NSObject <UIPageViewControllerDataSource>
+
+- (DataViewController *)viewControllerAtIndex:(NSUInteger)index storyboard:(UIStoryboard *)storyboard;
+- (NSUInteger)indexOfViewController:(DataViewController *)viewController;
+
+@end
+
