d61db3fd5363c85d6442dc0336d55e12b9f75885
STUDENT <STUDENT@CEAC-TCs-iMac-20.local>
2018-03-22 14:38:35 +0800

    Added template

---
+++ Schooly/Schooly/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  Schooly
+//
+//  Created by STUDENT on 22/03/2018.
+//  Copyright (c) 2018 Gillian Claire Cancio. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
---
+++ Schooly/Schooly/ViewController.m
@@ -0,0 +1,27 @@
+//
+//  ViewController.m
+//  Schooly
+//
+//  Created by STUDENT on 22/03/2018.
+//  Copyright (c) 2018 Gillian Claire Cancio. All rights reserved.
+//
+
+#import "ViewController.h"
+
+@interface ViewController ()
+
+@end
+
+@implementation ViewController
+
+- (void)viewDidLoad {
+    [super viewDidLoad];
+    // Do any additional setup after loading the view, typically from a nib.
+}
+
+- (void)didReceiveMemoryWarning {
+    [super didReceiveMemoryWarning];
+    // Dispose of any resources that can be recreated.
+}
+
+@end
